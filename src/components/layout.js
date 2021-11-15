21b23f0a284441fe2fca75236779b6b2036c38be
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2021-11-16 02:36:01 +0700

    :zap: Added linkedIn ref, major refactor in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,17 +4,67 @@ import Image from 'gatsby-image';
 
 import { rhythm, scale } from '../utils/typography';
 
+const styles = {
+  container: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    maxWidth: rhythm(24),
+    padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+  },
+  footer: {
+    container: {
+      display: 'flex',
+    },
+    referralLink: {
+      marginRight: rhythm(1 / 2),
+    },
+    date: {
+      marginTop: rhythm(1 / 4),
+      marginLeft: `auto`,
+      textAlign: `right`,
+    },
+  },
+  divider: {
+    marginBottom: rhythm(1),
+  },
+  homepageHeader: {
+    h1: {
+      ...scale(0.42),
+      marginBottom: rhythm(1.5),
+      marginTop: 0,
+    },
+    link: {
+      boxShadow: 'none',
+      color: 'inherit',
+    },
+  },
+  blogPostHeader: {
+    h3: {
+      fontFamily: 'Poppins, sans-serif',
+      fontWeight: 'normal',
+      marginTop: 0,
+    },
+    link: {
+      boxShadow: 'none',
+      color: 'inherit',
+    },
+  },
+};
+
 const Layout = ({ location, title, children }) => {
+  let header;
+  const rootPath = `${__PATH_PREFIX__}/`;
+
   const socialMedia = useStaticQuery(graphql`
     query socialMediaQuery {
-      instagram: file(absolutePath: { regex: "/ig.png/" }) {
+      github: file(absolutePath: { regex: "/git.png/" }) {
         childImageSharp {
           fixed(width: 40, height: 40) {
             ...GatsbyImageSharpFixed
           }
         }
       }
-      github: file(absolutePath: { regex: "/git.png/" }) {
+      instagram: file(absolutePath: { regex: "/ig.png/" }) {
         childImageSharp {
           fixed(width: 40, height: 40) {
             ...GatsbyImageSharpFixed
@@ -28,26 +78,43 @@ const Layout = ({ location, title, children }) => {
           }
         }
       }
+      linkedin: file(absolutePath: { regex: "/linkin.png/" }) {
+        childImageSharp {
+          fixed(width: 40, height: 40) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
     }
   `);
 
-  const rootPath = `${__PATH_PREFIX__}/`;
-  let header;
+  const links = [
+    {
+      link: 'https://github.com/padulkemid',
+      alt: 'github',
+      img: socialMedia.github.childImageSharp.fixed,
+    },
+    {
+      link: 'https://twitter.com/padulkemid',
+      alt: 'twitter',
+      img: socialMedia.twitter.childImageSharp.fixed,
+    },
+    {
+      link: 'https://instagram.com/padulkemid',
+      alt: 'instagram',
+      img: socialMedia.instagram.childImageSharp.fixed,
+    },
+    {
+      link: 'https://linkedin.com/in/padulkemid/',
+      alt: 'linkedin',
+      img: socialMedia.linkedin.childImageSharp.fixed,
+    },
+  ];
 
   if (location.pathname === rootPath) {
     header = (
-      <h1
-        style={{
-          ...scale(0.42),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}>
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}>
+      <h1 style={styles.homepageHeader.h1}>
+        <Link style={styles.homepageHeader.link} to={`/`}>
           + solve et coagula -
         </Link>
       </h1>
@@ -55,90 +122,28 @@ const Layout = ({ location, title, children }) => {
   } else {
     header = (
       <>
-        <h3
-          style={{
-            fontFamily: `Poppins, sans-serif`,
-            fontWeight: 'normal',
-            marginTop: 0,
-          }}>
-          <Link
-            style={{
-              boxShadow: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}>
+        <h3 style={styles.blogPostHeader.h3}>
+          <Link style={styles.blogPostHeader.link} to={`/`}>
             {title}
           </Link>
         </h3>
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
+        <hr style={styles.divider} />
       </>
     );
   }
+
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}>
+    <div style={styles.container}>
       <header>{header}</header>
       <main>{children}</main>
-      <hr
-        style={{
-          marginBottom: rhythm(1),
-        }}
-      />
-      <footer
-        style={{
-          display: `flex`,
-        }}>
-        <a
-          href="https://github.com/padulkemid"
-          target="_blank"
-          rel="noreferrer">
-          <Image
-            fixed={socialMedia.github.childImageSharp.fixed}
-            alt="github"
-            style={{
-              marginRight: rhythm(1 / 2),
-            }}
-          />
-        </a>
-        <a
-          href="https://twitter.com/padulkemid"
-          target="_blank"
-          rel="noreferrer">
-          <Image
-            fixed={socialMedia.twitter.childImageSharp.fixed}
-            alt="twitter"
-            style={{
-              marginRight: rhythm(1 / 2),
-            }}
-          />
-        </a>
-        <a
-          href="https://instagram.com/padulkemid"
-          target="_blank"
-          rel="noreferrer">
-          <Image
-            fixed={socialMedia.instagram.childImageSharp.fixed}
-            alt="instagram"
-            style={{
-              marginRight: rhythm(1 / 2),
-            }}
-          />
-        </a>
-        <p
-          style={{
-            marginTop: rhythm(1 / 4),
-            marginLeft: `auto`,
-            textAlign: `right`,
-          }}>
+      <hr style={styles.divider} />
+      <footer style={styles.footer.container}>
+        {links.map(({ link, img, alt }) => (
+          <a href={link} target="_blank" rel="noreferrer">
+            <Image fixed={img} alt={alt} style={styles.footer.referralLink} />
+          </a>
+        ))}
+        <p style={styles.footer.date}>
           &copy; {new Date().getFullYear()}, padulkemid
         </p>
       </footer>
